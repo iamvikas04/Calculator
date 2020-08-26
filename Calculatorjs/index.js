0ef217ecbd3cdfaa336748ea54c9fad9cdbaa49a
vikas <63897697+iamvikas04@users.noreply.github.com>
2020-08-26 09:23:32 -0700

    Add files via upload

---
+++ Calculatorjs/index.js
@@ -0,0 +1,31 @@
+
+function EqualButton() {
+  let ans = document.getElementById('output-value').innerText;
+  let fans = eval(ans);
+  document.getElementById('evaluation-value').innerText = "";
+  document.getElementById('output-value').innerText = fans;
+}
+
+function Equal() {
+  let ans = document.getElementById('output-value').innerText;
+  let fans = eval(ans);
+  document.getElementById('evaluation-value').innerText = fans;
+}
+
+function AddValue(value) {
+  document.getElementById('output-value').innerText += value;
+  Equal();
+}
+
+function clearText() {
+  document.getElementById('output-value').innerText = "";
+  document.getElementById('evaluation-value').innerText = "";
+}
+
+function backSpace() {
+  document.getElementById('output-value').innerText =   document.getElementById('output-value').innerText.substring(
+    0,  document.getElementById('output-value').innerText.length-1)
+    
+  // document.getElementById('evaluation-value').innerText =   document.getElementById('output-value').innerText.substring(
+  //   0,  document.getElementById('evaluation-value').innerText.length-1)
+}
